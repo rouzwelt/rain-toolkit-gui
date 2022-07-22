6df99fb4906df57fe192be93163c43ae563621d9
Mayur Nagare <mayurnagare@192.168.1.4>
2022-07-22 21:29:28 +0530

    added check for erc20TransferTier contract address

--- src/routes/combine-tier/HoldingTimeTransferTier.svelte
+++ src/routes/combine-tier/HoldingTimeTransferTier.svelte
@@ -4,23 +4,47 @@
   import FormPanel from "../../components/FormPanel.svelte";
   import Input from "../../components/Input.svelte";
   import { addressValidate } from "../../validation";
-  import Switch from "src/components/Switch.svelte";  
+  import Switch from "src/components/Switch.svelte";
   import ContractDeploy from "src/components/ContractDeploy.svelte";
-  import { CombineTier, CombineTierGenerator } from "rain-sdk";
+  import {
+    CombineTier,
+    CombineTierGenerator,
+    ERC20TransferTier,
+  } from "rain-sdk";
   import HumanReadable from "../../components/FriendlySource/HumanReadable.svelte";
+  import { queryStore } from "@urql/svelte";
+  import { client } from "src/stores";
 
-
-  let tierContract: string = "0x6ba1fadb694e806c316337143241dd6cfebd5033",
-		multipleCheck = false,
+  let tierContract: string = "0x2554d24a599decd2a1d5395ff9c78dc92d8ed24e",
+    multipleCheck = false,
     FriendlySource,
-    deployPromise: any;
-	
-	let blocks: any = [];
+    deployPromise: any,
+    tierContractError = null,
+    transferTier;
+
+  let blocks: any = [];
+
+  $: transferTier = queryStore({
+    client: $client,
+    query: `
+        query ($tierContract: Bytes!) {
+          erc20TransferTier (id: $tierContract) {
+            address
+          }
+        }`,
+    variables: { tierContract },
+    requestPolicy: "network-only",
+  });
+
+  $: tierContractError =
+    !$transferTier?.fetching && $transferTier?.data?.erc20TransferTier == null
+      ? "Not a valid ERC20TransferTier Contract Address"
+      : null;
 
   $: FriendlySource = {
     tierContract,
-    blocks
-  }
+    blocks,
+  };
 
   const deployCombineTier = async () => {
     const combineTierConfig = new CombineTierGenerator(
@@ -39,10 +63,13 @@
 <div class="flex w-full gap-x-3">
   <div class="flex w-3/5 flex-col gap-y-4">
     <div class="mb-2 flex flex-col gap-y-2">
-      <span class="text-2xl">Deploy a new "Has Tier Been Held For Blocks" from a TransferTier.</span>
+      <span class="text-2xl"
+        >Deploy a new "Has Tier Been Held For Blocks" from a TransferTier.</span
+      >
       <span class="text-gray-400">
-				Choose a TransferTier contract address to deploy a CombineTier that will produce a report out of if the tiers
-				have been held for x number of blocks or not.
+        Choose a TransferTier contract address to deploy a CombineTier that will
+        produce a report out of if the tiers have been held for x number of
+        blocks or not.
       </span>
     </div>
     <FormPanel heading="Settings">
@@ -51,58 +78,69 @@
         placeholder="Tier address"
         bind:value={tierContract}
         validator={addressValidate}
+        errorMsg={tierContractError}
       >
         <span slot="label">TransferTier Contract</span>
       </Input>
     </FormPanel>
-		<FormPanel>
-			<div>
-				<span> Tier Blocks: </span>
-				<Switch
-					bind:checked={multipleCheck}
-					on:change={() => {
-						if (multipleCheck) {
-							document.getElementById("multiple").style.display = "block";
-							document.getElementById("single").style.display = "none";
-						}
-						else {
-							document.getElementById("multiple").style.display = "none";
-							document.getElementById("single").style.display = "block";
-						}
-					}}
-				/>
-				<br />
-				<span class="text-gray-400"
-					>if switched off only one value will be used for all 8 tiers to compare against TransferTier report.<br>
-					if switched on each tier can have its own value to be compared agaoanst TransferTier report, however each provided value should be less than or equal to the previous one.</span
-				>
-			</div>
-			<div id="single" style="display:none">
-				<Input type="number" placeholder="Tier 1" bind:value={blocks[0]}>
-					<span slot="label"
-						>Set the time span in blocks for each to cpare against the TransferTier report.</span
-					>
-				</Input>
-			</div>
-			<div id="multiple" class="flex w-full flex-col space-y-3" style="display:none">
-				<Input type="number" placeholder="Tier 1" bind:value={blocks[0]}>
-					<span slot="label"
-						>Set the time span in blocks for each tier which will be compared against the TransferTier report.</span
-					>
-				</Input>
-				<Input type="number" placeholder="Tier 2" bind:value={blocks[1]} />
-				<Input type="number" placeholder="Tier 3" bind:value={blocks[2]} />
-				<Input type="number" placeholder="Tier 4" bind:value={blocks[3]} />
-				<Input type="number" placeholder="Tier 5" bind:value={blocks[4]} />
-				<Input type="number" placeholder="Tier 6" bind:value={blocks[5]} />
-				<Input type="number" placeholder="Tier 7" bind:value={blocks[6]} />
-				<Input type="number" placeholder="Tier 8" bind:value={blocks[7]} />
-			</div>
-		</FormPanel>
+    <FormPanel>
+      <div>
+        <span> Tier Blocks: </span>
+        <Switch
+          bind:checked={multipleCheck}
+          on:change={() => {
+            if (multipleCheck) {
+              document.getElementById("multiple").style.display = "block";
+              document.getElementById("single").style.display = "none";
+            } else {
+              document.getElementById("multiple").style.display = "none";
+              document.getElementById("single").style.display = "block";
+            }
+          }}
+        />
+        <br />
+        <span class="text-gray-400"
+          >if switched off only one value will be used for all 8 tiers to
+          compare against TransferTier report.<br />
+          if switched on each tier can have its own value to be compared agaoanst
+          TransferTier report, however each provided value should be less than or
+          equal to the previous one.</span
+        >
+      </div>
+      <div id="single" style="display:none">
+        <Input type="number" placeholder="Tier 1" bind:value={blocks[0]}>
+          <span slot="label"
+            >Set the time span in blocks for each to cpare against the
+            TransferTier report.</span
+          >
+        </Input>
+      </div>
+      <div
+        id="multiple"
+        class="flex w-full flex-col space-y-3"
+        style="display:none"
+      >
+        <Input type="number" placeholder="Tier 1" bind:value={blocks[0]}>
+          <span slot="label"
+            >Set the time span in blocks for each tier which will be compared
+            against the TransferTier report.</span
+          >
+        </Input>
+        <Input type="number" placeholder="Tier 2" bind:value={blocks[1]} />
+        <Input type="number" placeholder="Tier 3" bind:value={blocks[2]} />
+        <Input type="number" placeholder="Tier 4" bind:value={blocks[3]} />
+        <Input type="number" placeholder="Tier 5" bind:value={blocks[4]} />
+        <Input type="number" placeholder="Tier 6" bind:value={blocks[5]} />
+        <Input type="number" placeholder="Tier 7" bind:value={blocks[6]} />
+        <Input type="number" placeholder="Tier 8" bind:value={blocks[7]} />
+      </div>
+    </FormPanel>
 
     <FormPanel>
       {#if !deployPromise}
-        <Button shrink on:click={handleClick}>Deploy CombineTier</Button>
+        <Button shrink on:click={handleClick} disabled={tierContractError}
+          >Deploy CombineTier</Button
+        >
       {:else}
         <ContractDeploy {deployPromise} type="CombineTier" />
       {/if}
@@ -115,7 +153,7 @@
           <HumanReadable
             signer={$signerAddress}
             contractType="combineTier"
-           {FriendlySource}
+            {FriendlySource}
           />
         </FormPanel>
       </span>
